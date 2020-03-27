41653bcaf1757a81abb359baa1a3df2c4b65aab5
Kyte Gurner <kyte.gurner@gmail.com>
2020-03-27 21:57:54 +1100

    cont

--- policy.py
+++ policy.py
@@ -3,14 +3,11 @@ defines the policy class
 """
 
 class policy(object):
-    def __init__(charge_ev = 0, charge_battery = 0, flexi_load = 0, discharge_ev = 0, discharge_battery = 0):
+    def __init__(charge_ev = 0, charge_battery = 0, flexi_load = 0):
         self.charge_ev = charge_ev
         self.charge_battery = charge_battery
         self.flexi_load = flexi_load
 
-        self.discharge_ev = discharge_ev
-        self.discharge_battery = discharge_battery
-
     def update(self, state):
         #This is where we use the trained neural net to update the policy given a new state
-        return 
+        return
--- state.py
+++ state.py
@@ -13,25 +13,26 @@ class state(object):
         #(this will be useful in update(), where we must look at the time of day to update info like vahicle_at_home)
         self.time = time
 
-        self.ev_leave_probabilities = ev_leave_probabilities
-        self.ev_return_probabilities = ev_return_probabilities
-
         #this is the policy to be applied in the next time interval.
         self.policy = policy
 
+        #these are the state variables that are inputs into the policy function
         self.ev_at_home = ev_at_home
+        self.ev_charge = ev_charge
+        self.bat_charge = bat_charge
+        self.flexi_charge = flexi_charge
 
+        #variables that are quasi-randomly generated (with time taken into account).
+        self.solar_generated = solar_generated
+        self.house_power_draw = house_power_draw
 
         #constants
         self.EV_CAPACITY = ev_capacity
         self.BATTERY_CAPACITY = battery_capacity
         self.VARIABLE_LOAD_POWER_REQ = variable_load_power_req
         self.SOLAR_GENERATION_CAPACITY = solar_generation_capacity
-
-
-        #variables that
-        self.solar_generated = solar_generated
-        self.house_power_draw = house_power_draw
+        self.EV_LEAVE_PROBABILITIES = ev_leave_probabilities
+        self.EV_RETURN_PROBABILITIES = ev_return_probabilities
 
     def update(self, policy):
         #stocastically update the state to the next state given the current state and input policy.
@@ -45,14 +46,14 @@ class state(object):
 
         def update_ev_at_home():
             if self.ev_at_home:
-                ev_leave_prob = self.ev_leave_probabilities[self.time * 2]
+                ev_leave_prob = self.EV_LEAVE_PROBABILITIES[self.time * 2]
                 if random.randint(10000) <= ev_leave_prob*10000:
                     #ev is at home
                     ev_at_home = True
                 else:
                     ev_at_home = False
             else:
-                ev_return_prob = self.ev_leave_probabilities[self.time*2]
+                ev_return_prob = self.EV_RETURN_PROBABILITIES[self.time*2]
                 if random.randint(10000) <= ev_return_prob*10000:
                     #ev returned home
                     ev_at_home = True
